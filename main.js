4392c179a44a2ab5ac0a7fd850a89d5ecdc422b9
Musawer Khan <musawerbangash109@gmail.com>
2024-04-26 20:45:52 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Comparison Operators
+let a = 10;
+let b = 10;
+let sum = a == b;
+console.log(sum);
+//Another Example
+let c = 2;
+let d = 3;
+let sum1 = c != d;
+console.log(sum1);
---
+++ main.ts
@@ -0,0 +1,14 @@
+    //Comparison Operators
+
+    let a:number = 10;
+    let b:number = 10;
+    let sum = a == b;
+    console.log(sum)
+
+    //Another Example
+
+    let c:number = 2;
+    let d:number = 3;
+    let sum1 = c != d
+    console.log(sum1)
+
